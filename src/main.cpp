69bad8465ff47bb428391945cd4231638fce7d9b
Thomas <tfk@debian>
2023-11-01 11:36:44 +0100

    removed default productId, deviceId, privateKey, serverurl

--- src/main.cpp
+++ src/main.cpp
@@ -85,12 +85,12 @@ void parse_options(int argc, char** argv, json& opts)
     {
         cxxopts::Options options(argv[0], "Nabto Webrtc Device example");
         options.add_options()
-            ("s,serverurl", "Optional. Server URL for the Nabto basestation", cxxopts::value<std::string>()->default_value("pr-4nmagfvj.devices.dev.nabto.net"))
-            ("d,deviceid", "Device ID to connect to", cxxopts::value<std::string>()->default_value("de-sw9unmnn"))
-            ("p,productid", "Product ID to use", cxxopts::value<std::string>()->default_value("pr-4nmagfvj"))
+            ("s,serverurl", "Optional. Server URL for the Nabto basestation", cxxopts::value<std::string>())
+            ("d,deviceid", "Device ID to connect to", cxxopts::value<std::string>())
+            ("p,productid", "Product ID to use", cxxopts::value<std::string>())
             ("t,sct", "Optional. Server connect token from device used for remote connect", cxxopts::value<std::string>()->default_value("demosct"))
             ("loglevel", "Optional. The log level (error|info|trace)", cxxopts::value<std::string>()->default_value("info"))
-            ("k,privatekey", "Raw private key to use", cxxopts::value<std::string>()->default_value("b5b45deb271a63071924a219a42b0b67146e50f15e2147c9c5b28f7cf9d1015d"))
+            ("k,privatekey", "Raw private key to use", cxxopts::value<std::string>())
             ("r,rtsp", "Use RTSP at the provided url instead of RTP (eg. rtsp://127.0.0.l:8554/video)", cxxopts::value<std::string>())
             ("rtpport", "Port number to use if NOT using RTSP", cxxopts::value<uint16_t>()->default_value("6000"))
             ("j,jwksurl", "URL for jwks server for oAuth", cxxopts::value<std::string>()->default_value("http://localhost:3000/jwks"))
@@ -105,10 +105,22 @@ void parse_options(int argc, char** argv, json& opts)
             exit(0);
         }
 
+        if (!result.count("productid") ||
+            !result.count("deviceid") ||
+            !result.count("privatekey"))
+            {
+                std::cout << "Missing required argument" << std::endl;
+                std::cout << options.help({ "", "Group" }) << std::endl;
+                exit(1);
+            }
         opts["productId"] = result["productid"].as<std::string>();
         opts["deviceId"] = result["deviceid"].as<std::string>();
-        opts["serverUrl"] = result["serverurl"].as<std::string>();
         opts["rawPrivateKey"] = result["privatekey"].as<std::string>();
+
+        if (result.count("serverurl")) {
+            opts["serverUrl"] = result["serverurl"].as<std::string>();
+        }
+
         opts["sct"] = result["sct"].as<std::string>();
         opts["logLevel"] = result["loglevel"].as<std::string>();
         if (result.count("rtsp")) {
