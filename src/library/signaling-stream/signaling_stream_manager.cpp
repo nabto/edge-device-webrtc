52119b2e259475a2c61f1f3209b76afc3b1c0928
Thomas <tfk@debian>
2024-01-02 14:20:30 +0100

    minor todo

--- src/library/signaling-stream/signaling_stream_manager.cpp
+++ src/library/signaling-stream/signaling_stream_manager.cpp
@@ -33,8 +33,13 @@ bool SignalingStreamManager::start()
             nabto_device_connection_get_client_fingerprint(self->device_.get(), ref, &fp);
             std::cout << "Creating Signaling stream for client fp: " << fp << std::endl;
             nabto_device_string_free(fp);
-            // TODO: callbacks should be to internal function so if the user changes them we do not have to update the callbacks in every signaling stream instance
-            SignalingStreamPtr s = SignalingStream::create(self->device_, stream, self, self->queue_, self->trackCb_, self->accessCb_);
+            SignalingStreamPtr s = SignalingStream::create(self->device_, stream, self, self->queue_,
+                [self](NabtoDeviceConnectionRef connRef, MediaTrackPtr track) {
+                    self->trackCb_(connRef, track);
+                },
+                [self](NabtoDeviceConnectionRef ref, std::string action) -> bool {
+                    return self->accessCb_(ref, action);
+                });
             self->streams_.push_back(s);
             s->start();
         }
