32a9a62433f929043ce0bc889a24779b8f8e4992
Thomas <tfk@debian>
2024-02-06 15:47:39 +0100

    add boost unit test framework

---
+++ test/signaling-tests/signaling_tests.cpp
@@ -0,0 +1,11 @@
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(signaling_api)
+
+BOOST_AUTO_TEST_CASE(aswer_to_offer, *boost::unit_test::timeout(180))
+{
+    BOOST_TEST(true);
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
