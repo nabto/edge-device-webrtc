8502fa2642f4b6e3d4642a0ada6684016ba2f913
Thomas <tfk@debian>
2024-02-08 13:33:41 +0100

    get turn server unittest

--- test/signaling-tests/signaling_tests.cpp
+++ test/signaling-tests/signaling_tests.cpp
@@ -11,7 +11,173 @@
 namespace nabto {
 namespace test {
 
-class TestDevice {
+static std::vector<uint8_t> jsonToStreamBuffer(nlohmann::json in) {
+    auto inStr = in.dump();
+    auto inStrLen = inStr.size();
+    std::vector<uint8_t> out;
+    out.push_back(inStrLen & 0xFF);
+    out.push_back((inStrLen & 0xFF00) >> 8);
+    out.push_back((inStrLen & 0xFF0000) >> 16);
+    out.push_back((inStrLen & 0xFF000000) >> 24);
+    for (size_t i = 0; i < inStrLen; i++) {
+        out.push_back(inStr[i]);
+    }
+    return out;
+}
+
+static nlohmann::json streamBufferToJson(uint8_t* buff, size_t len) {
+    std::string str((char*)buff, len);
+    return nlohmann::json::parse(str);
+}
+
+class VirtualCoap {
+public:
+    VirtualCoap(NabtoDevicePtr device, NabtoDeviceVirtualConnection* conn, NabtoDeviceCoapMethod method, const char* path) {
+        req_ = nabto_device_virtual_coap_request_new(conn, method, path);
+        future_ = nabto_device_future_new(device.get());
+    }
+
+    ~VirtualCoap() {
+        nabto_device_virtual_coap_request_free(req_);
+        nabto_device_future_free(future_);
+    }
+
+    void execute(std::function<void(NabtoDeviceError ec)> cb) {
+        cb_ = cb;
+        nabto_device_virtual_coap_request_execute(req_, future_);
+        nabto_device_future_set_callback(future_, responseReady, this);
+    }
+
+    nlohmann::json getJsonResp() {
+        uint16_t cf;
+        BOOST_TEST(nabto_device_virtual_coap_request_get_response_content_format(req_, &cf) == NABTO_DEVICE_EC_OK);
+        BOOST_TEST(cf == NABTO_DEVICE_COAP_CONTENT_FORMAT_APPLICATION_JSON);
+
+        char* payload;
+        size_t len;
+        BOOST_TEST(nabto_device_virtual_coap_request_get_response_payload(req_, (void**)&payload, &len) == NABTO_DEVICE_EC_OK);
+
+        std::string pl(payload, len);
+        try {
+            auto json = nlohmann::json::parse(pl);
+            return json;
+        } catch (std::exception& ex) {
+            BOOST_TEST(ex.what() == "");
+            return nlohmann::json();
+        }
+    }
+
+    NabtoDeviceVirtualCoapRequest* getRequest() { return req_; }
+
+private:
+    static void responseReady(NabtoDeviceFuture* future, NabtoDeviceError ec, void* userData)
+    {
+        VirtualCoap* self = (VirtualCoap*)userData;
+        self->cb_(ec);
+        self->cb_ = nullptr;
+    }
+
+    NabtoDeviceVirtualCoapRequest* req_;
+    NabtoDeviceFuture* future_ = NULL;
+    std::function<void(NabtoDeviceError ec)> cb_;
+
+};
+
+class VirtualStream {
+public:
+    VirtualStream(NabtoDevicePtr device, NabtoDeviceVirtualConnection* conn) {
+        stream_ = nabto_device_virtual_stream_new(conn);
+        future_ = nabto_device_future_new(device.get());
+        device_ = device;
+    }
+
+    ~VirtualStream() {
+        nabto_device_virtual_stream_free(stream_);
+        nabto_device_future_free(future_);
+
+    }
+
+    void open(uint32_t port, std::function<void(NabtoDeviceError ec)> cb) {
+        openCb_ = cb;
+        nabto_device_virtual_stream_open(stream_, future_, port);
+        nabto_device_future_set_callback(future_, streamOpened, this);
+
+    }
+
+    void write(std::vector<uint8_t>& data, std::function<void(NabtoDeviceError ec)> cb) {
+        writeCb_ = cb;
+        auto fut = nabto_device_future_new(device_.get());
+        nabto_device_virtual_stream_write(stream_, fut, data.data(), data.size());
+        nabto_device_future_set_callback(fut, streamWritten, this);
+    }
+
+    void readAll(size_t len, std::function<void(NabtoDeviceError ec, uint8_t* buff, size_t len)> cb) {
+        readCb_ = cb;
+        auto fut = nabto_device_future_new(device_.get());
+        nabto_device_virtual_stream_read_all(stream_, fut, readBuffer_, len, &readLen_);
+        nabto_device_future_set_callback(fut, streamRead, this);
+
+    }
+
+    void close(std::function<void(NabtoDeviceError ec)> cb) {
+        closeCb_ = cb;
+        nabto_device_virtual_stream_close(stream_, future_);
+        nabto_device_future_set_callback(future_, streamClosed, this);
+
+    }
+
+    void abort() {
+        nabto_device_virtual_stream_abort(stream_);
+    }
+
+private:
+    static void streamOpened(NabtoDeviceFuture* future, NabtoDeviceError ec, void* userData)
+    {
+        VirtualStream* self = (VirtualStream*)userData;
+        self->openCb_(ec);
+        self->openCb_ = nullptr;
+    }
+
+    static void streamClosed(NabtoDeviceFuture* future, NabtoDeviceError ec, void* userData)
+    {
+        VirtualStream* self = (VirtualStream*)userData;
+        self->closeCb_(ec);
+        self->closeCb_ = nullptr;
+    }
+
+    static void streamWritten(NabtoDeviceFuture* future, NabtoDeviceError ec, void* userData)
+    {
+        nabto_device_future_free(future);
+        VirtualStream* self = (VirtualStream*)userData;
+        auto cb = self->writeCb_;
+        self->writeCb_ = nullptr;
+        cb(ec);
+    }
+
+    static void streamRead(NabtoDeviceFuture* future, NabtoDeviceError ec, void* userData)
+    {
+        nabto_device_future_free(future);
+        VirtualStream* self = (VirtualStream*)userData;
+        auto cb = self->readCb_;
+        self->readCb_ = nullptr;
+        cb(ec, self->readBuffer_, self->readLen_);
+    }
+
+    NabtoDeviceVirtualStream* stream_;
+    NabtoDeviceFuture* future_ = NULL;
+    NabtoDevicePtr device_;
+    std::function<void(NabtoDeviceError ec)> openCb_;
+    std::function<void(NabtoDeviceError ec)> closeCb_;
+    std::function<void(NabtoDeviceError ec, uint8_t* buff, size_t len)> readCb_;
+    std::function<void(NabtoDeviceError ec)> writeCb_;
+
+    uint8_t readBuffer_[1024];
+    size_t readLen_;
+
+};
+
+
+class TestDevice : public std::enable_shared_from_this<TestDevice> {
 public:
     TestDevice() :
         device_(nabto::makeNabtoDevice()),
@@ -75,8 +241,27 @@ public:
         return connection_;
     }
 
+    void makeConnectionSigStream(std::function<void(NabtoDeviceVirtualConnection* conn, std::shared_ptr<VirtualStream> stream)> cb) {
+        conn_ = makeConnection();
+        auto self = shared_from_this();
+        coap_ = std::make_shared <VirtualCoap>(device_, conn_, NABTO_DEVICE_COAP_GET, "/webrtc/info");
+        stream_ = std::make_shared<VirtualStream>(device_, conn_);
+
+        coap_->execute([self, cb](NabtoDeviceError ec) {
+            auto resp = self->coap_->getJsonResp();
+            auto port = resp["SignalingStreamPort"].get<uint32_t>();
+            self->stream_->open(port, [self, cb](NabtoDeviceError ec) {
+                BOOST_TEST(ec == NABTO_DEVICE_EC_OK);
+                cb(self->conn_, self->stream_);
+                self->conn_ = NULL;
+                self->stream_ = nullptr;
+                self->coap_ = nullptr;
+            });
+
+        });
+    }
+
     void run() {
-        std::cout << "adding work" << std::endl;
         eventQueue_->addWork();
         eventQueue_->run();
     }
@@ -93,13 +278,13 @@ public:
         eventQueue_->post([device]() {
             nabto_device_stop(device);
             });
-        std::cout << "Removing work" << std::endl;
         eventQueue_->removeWork();
     }
 
 private:
 
     static void deviceClosed(NabtoDeviceFuture* future, NabtoDeviceError ec, void* userData) {
+        nabto_device_future_free(future);
         TestDevice* self = (TestDevice*)userData;
         self->stop();
     }
@@ -108,121 +293,11 @@ private:
     nabto::EventQueueImplPtr eventQueue_;
     nabto::NabtoDeviceWebrtcPtr webrtc_;
     std::string deviceFp_;
-
+    std::shared_ptr<VirtualStream> stream_ = nullptr;
+    std::shared_ptr<VirtualCoap> coap_ = nullptr;
+    NabtoDeviceVirtualConnection* conn_ = NULL;
 };
 
-
-class VirtualCoap {
-public:
-    VirtualCoap(NabtoDevicePtr device, NabtoDeviceVirtualConnection* conn, NabtoDeviceCoapMethod method, const char* path) {
-        req_ = nabto_device_virtual_coap_request_new(conn, method, path);
-        future_ = nabto_device_future_new(device.get());
-    }
-
-    ~VirtualCoap() {
-        nabto_device_virtual_coap_request_free(req_);
-        nabto_device_future_free(future_);
-    }
-
-    void execute(std::function<void (NabtoDeviceError ec)> cb) {
-        cb_ = cb;
-        nabto_device_virtual_coap_request_execute(req_, future_);
-        nabto_device_future_set_callback(future_, responseReady, this);
-    }
-
-    nlohmann::json getJsonResp() {
-        uint16_t cf;
-        BOOST_TEST(nabto_device_virtual_coap_request_get_response_content_format(req_, &cf) == NABTO_DEVICE_EC_OK);
-        BOOST_TEST(cf == NABTO_DEVICE_COAP_CONTENT_FORMAT_APPLICATION_JSON);
-
-        char* payload;
-        size_t len;
-        BOOST_TEST(nabto_device_virtual_coap_request_get_response_payload(req_, (void**)&payload, &len) == NABTO_DEVICE_EC_OK);
-
-        std::string pl(payload, len);
-        try {
-            auto json = nlohmann::json::parse(pl);
-            return json;
-        } catch (std::exception& ex) {
-            BOOST_TEST(ex.what() == "");
-            return nlohmann::json();
-        }
-    }
-
-    NabtoDeviceVirtualCoapRequest* getRequest() {return req_;}
-
-private:
-    static void responseReady(NabtoDeviceFuture* future, NabtoDeviceError ec, void* userData)
-    {
-        VirtualCoap* self = (VirtualCoap*)userData;
-        self->cb_(ec);
-
-    }
-
-    NabtoDeviceVirtualCoapRequest* req_;
-    NabtoDeviceFuture* future_ = NULL;
-    std::function<void(NabtoDeviceError ec)> cb_;
-
-};
-
-class VirtualStream {
-public:
-    VirtualStream(NabtoDevicePtr device, NabtoDeviceVirtualConnection* conn) {
-        stream_ = nabto_device_virtual_stream_new(conn);
-        future_ = nabto_device_future_new(device.get());
-
-    }
-
-    ~VirtualStream() {
-        std::cout << "Virtual stream descructor" << std::endl;
-        nabto_device_virtual_stream_free(stream_);
-        nabto_device_future_free(future_);
-
-    }
-
-    void open(uint32_t port, std::function<void(NabtoDeviceError ec)> cb) {
-        std::cout << "Stream open" << std::endl;
-
-        openCb_ = cb;
-        nabto_device_virtual_stream_open(stream_, future_, port);
-        nabto_device_future_set_callback(future_, streamOpened, this);
-
-    }
-
-    void close(std::function<void(NabtoDeviceError ec)> cb) {
-        closeCb_ = cb;
-        nabto_device_virtual_stream_close(stream_, future_);
-        nabto_device_future_set_callback(future_, streamClosed, this);
-
-    }
-
-    void abort() {
-        nabto_device_virtual_stream_abort(stream_);
-    }
-
-private:
-    static void streamOpened(NabtoDeviceFuture* future, NabtoDeviceError ec, void* userData)
-    {
-        std::cout << "Stream opened callback 1" << std::endl;
-        VirtualStream* self = (VirtualStream*)userData;
-        self->openCb_(ec);
-    }
-
-    static void streamClosed(NabtoDeviceFuture* future, NabtoDeviceError ec, void* userData)
-    {
-        std::cout << "Stream closed callback" << std::endl;
-        VirtualStream* self = (VirtualStream*)userData;
-        self->closeCb_(ec);
-    }
-
-    NabtoDeviceVirtualStream* stream_;
-    NabtoDeviceFuture* future_ = NULL;
-    std::function<void(NabtoDeviceError ec)> openCb_;
-    std::function<void(NabtoDeviceError ec)> closeCb_;
-    std::function<void(NabtoDeviceError ec)> readCb_;
-    std::function<void(NabtoDeviceError ec)> writeCb_;
-
-};
 
 }
 } // namespace
@@ -232,12 +307,12 @@ BOOST_AUTO_TEST_SUITE(signaling_api)
 
 BOOST_AUTO_TEST_CASE(get_stream_port, *boost::unit_test::timeout(180))
 {
-    auto td = nabto::test::TestDevice();
-    auto conn = td.makeConnection();
+    auto td = std::make_shared<nabto::test::TestDevice>();
+    auto conn = td->makeConnection();
 
-    auto coap = nabto::test::VirtualCoap(td.getDevice(), conn, NABTO_DEVICE_COAP_GET, "/webrtc/info");
+    auto coap = nabto::test::VirtualCoap(td->getDevice(), conn, NABTO_DEVICE_COAP_GET, "/webrtc/info");
 
-    coap.execute([&td, &coap](NabtoDeviceError ec) {
+    coap.execute([td, &coap](NabtoDeviceError ec) {
         BOOST_TEST(ec == NABTO_DEVICE_EC_OK);
         auto req = coap.getRequest();
         uint16_t status;
@@ -261,42 +336,86 @@ BOOST_AUTO_TEST_CASE(get_stream_port, *boost::unit_test::timeout(180))
             BOOST_TEST(ex.what() == "");
         }
 
-        td.stop();
+        td->stop();
     });
 
-    td.run();
+    td->run();
 
     nabto_device_virtual_connection_free(conn);
 }
 
 BOOST_AUTO_TEST_CASE(open_signaling_stream, *boost::unit_test::timeout(180))
 {
-    const std::string clientFp = "1234567890123456789012345678901212345678901234567890123456789012";
+    auto td = std::make_shared<nabto::test::TestDevice>();
+    auto conn = td->makeConnection();
 
-    auto td = nabto::test::TestDevice();
-    auto conn = td.makeConnection();
-    nabto_device_virtual_connection_set_client_fingerprint(conn, clientFp.c_str());
+    auto coap = nabto::test::VirtualCoap(td->getDevice(), conn, NABTO_DEVICE_COAP_GET, "/webrtc/info");
+    auto stream = nabto::test::VirtualStream(td->getDevice(), conn);
 
-    auto coap = nabto::test::VirtualCoap(td.getDevice(), conn, NABTO_DEVICE_COAP_GET, "/webrtc/info");
-    auto stream = nabto::test::VirtualStream(td.getDevice(), conn);
-
-    coap.execute([&td, &conn, &coap, &stream](NabtoDeviceError ec) {
+    coap.execute([td, &conn, &coap, &stream](NabtoDeviceError ec) {
         auto resp = coap.getJsonResp();
         auto port = resp["SignalingStreamPort"].get<uint32_t>();
-        std::cout << "Got signaling port: " << port << " opening stream" << std::endl;
-        stream.open(port, [&td, &stream](NabtoDeviceError ec){
-            std::cout << "Stream opened callback 2" << std::endl;
+        stream.open(port, [td, &stream](NabtoDeviceError ec) {
             BOOST_TEST(ec == NABTO_DEVICE_EC_OK);
             stream.abort();
-            td.close();
-            std::cout << "device stopped" << std::endl;
+            td->close();
+            });
+
         });
 
-    });
-
-    td.run();
+    td->run();
 
     nabto_device_virtual_connection_free(conn);
+}
+
+BOOST_AUTO_TEST_CASE(open_signaling_stream_helper, *boost::unit_test::timeout(180))
+{
+    auto td = std::make_shared<nabto::test::TestDevice>();
+    td->makeConnectionSigStream([td](NabtoDeviceVirtualConnection* conn, std::shared_ptr<nabto::test::VirtualStream> stream) {
+        stream->abort();
+        td->close();
+        nabto_device_virtual_connection_free(conn);
+
+        });
+
+    td->run();
+
+}
+
+BOOST_AUTO_TEST_CASE(get_turn_servers, *boost::unit_test::timeout(180))
+{
+    auto td = std::make_shared<nabto::test::TestDevice>();
+    std::vector<uint8_t> req;
+    td->makeConnectionSigStream([td, &req](NabtoDeviceVirtualConnection* conn, std::shared_ptr<nabto::test::VirtualStream> stream) {
+        nlohmann::json turnReqjson = {{"type", 3}};
+        req = nabto::test::jsonToStreamBuffer(turnReqjson);
+
+        stream->write(req, [stream, td, conn](NabtoDeviceError ec) {
+            BOOST_TEST(ec == NABTO_DEVICE_EC_OK);
+            stream->readAll(4, [stream, td, conn](NabtoDeviceError ec, uint8_t* buff, size_t len) {
+                BOOST_TEST(ec == NABTO_DEVICE_EC_OK);
+                uint32_t l = *((uint32_t*)buff);
+                stream->readAll(l, [stream, td, conn, l](NabtoDeviceError ec, uint8_t* buff, size_t len) {
+                    BOOST_TEST(ec == NABTO_DEVICE_EC_OK);
+                    auto resp = nabto::test::streamBufferToJson(buff, len);
+
+                    auto type = resp["type"].get<uint32_t>();
+                    BOOST_TEST(type == 4);
+
+                    auto servs = resp["servers"].get<std::vector<nlohmann::json> >();
+                    BOOST_TEST(servs.size() == 0);
+
+                    stream->abort();
+                    td->close();
+                    nabto_device_virtual_connection_free(conn);
+
+                    });
+            });
+        });
+    });
+
+    td->run();
+
 }
 
 
