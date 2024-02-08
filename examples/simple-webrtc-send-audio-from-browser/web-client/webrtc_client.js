051a31c69f5735b5a28d4b927a88991fcafa130a
Michael <mkm@nabto.com>
2024-02-08 08:12:56 +0100

    send audio from browser to the device example

---
+++ examples/simple-webrtc-send-audio-from-browser/web-client/webrtc_client.js
@@ -0,0 +1,93 @@
+
+// import EdgeWebrtcConnectionFactory from "./edge-webrtc/edge_webrtc";
+
+// var edgeWebrtc = require("./edge_webrtc_bundle")
+//var edgeWebrtc = globalThis.window.NabtoEdgeWebrtcFactory;
+
+var productId = null;
+var deviceId = null;
+var sct = null;
+
+var webrtcConnection = null;
+var connected = false;
+
+function connect()
+{
+  webrtcConnection = globalThis.window.EdgeWebrtc.EdgeWebrtcConnectionFactory.create();
+  productId = document.getElementById("productid").value;
+  deviceId = document.getElementById("deviceid").value;
+  sct = document.getElementById("sct").value;
+
+  webrtcConnection.setConnectionOptions({productId: productId, deviceId: deviceId, sct: sct, signalingServerUrl: "wss://signaling.smartcloud.nabto.com"});
+
+  webrtcConnection.onTrack((event, trackId) => {
+  });
+
+  webrtcConnection.onConnected(() => {
+    connected = true;
+  });
+
+  webrtcConnection.connect();
+}
+
+async function downstreamAudio() {
+  const constraints = window.constraints = {
+    audio: true,
+    video: false
+  };
+  let stream = await navigator.mediaDevices.getUserMedia(constraints);
+
+  let auTrack = stream.getAudioTracks();
+
+  if (auTrack.length > 0) {
+    await webrtcConnection.addTrack(auTrack[0], "frontdoor-audio");
+  }
+}
+
+async function downstreamVideo() {
+  const constraints = window.constraints = {
+    audio: false,
+    video: true
+  };
+  let stream = await navigator.mediaDevices.getUserMedia(constraints);
+
+  let track = stream.getVideoTracks();
+
+  if (track.length > 0) {
+    await webrtcConnection.addTrack(track[0], "frontdoor-video");
+  }
+}
+
+
+addEventListener("load", (event) => {
+  updateInfo();
+});
+
+function storeInfo() {
+  productId = document.getElementById("productid").value;
+  deviceId = document.getElementById("deviceid").value;
+  sct = document.getElementById("sct").value;
+
+  localStorage.setItem("productId", productId);
+  localStorage.setItem("deviceId", deviceId);
+  localStorage.setItem("sct", sct);
+  updateInfo();
+}
+
+function updateInfo() {
+  const prod = localStorage.getItem("productId");
+  if (prod) {
+    document.getElementById("productid").value = prod;
+    productId = prod;
+  }
+  const dev = localStorage.getItem("deviceId");
+  if (dev) {
+    document.getElementById("deviceid").value = dev;
+    deviceId = dev;
+  }
+  const sctLoc = localStorage.getItem("sct");
+  if (sctLoc) {
+    document.getElementById("sct").value = sctLoc;
+    sct = sctLoc;
+  }
+};
