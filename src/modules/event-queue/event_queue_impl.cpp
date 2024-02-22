703cb3c72a0e40718a163ea5e9667350841f00b7
Thomas <tfk@debian>
2024-02-22 11:06:10 +0100

    switch from recursion to while true

--- src/modules/event-queue/event_queue_impl.cpp
+++ src/modules/event-queue/event_queue_impl.cpp
@@ -40,7 +40,7 @@ void EventQueueImpl::removeWork()
 
 QueueEvent EventQueueImpl::pop()
 {
-    {
+    while (true) {
         std::unique_lock<std::mutex> lock(mutex_);
         // If no events AND (outstanding workers OR not stopped), wait for events
         if (events_.empty() && workCount_ > 0) {
@@ -54,10 +54,8 @@ QueueEvent EventQueueImpl::pop()
         } else if (workCount_ < 1) {// Else if queue has no workers, stop
             return nullptr;
         }
+        // work was removed which broke the wait, however, it was not the last work, and there was no events to handle, going back to wait
     }
-    // work was removed which broke the wait, however, it was not the last work, and there was no events to handle, going back to wait
-    return pop();
-
     // // This should not happen!
     // std::cout << "THIS SHOULD NOT HAPPEN! Wait done queue empty, workCount: " << workCount_ << std::endl;
     // return nullptr;
