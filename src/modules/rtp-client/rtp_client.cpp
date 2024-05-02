5e185fbb5fb3e8400d6d2125eb718b18024b1e22
Thomas <tfk@debian>
2024-05-02 10:22:16 +0200

    readd missing lock guard

--- src/modules/rtp-client/rtp_client.cpp
+++ src/modules/rtp-client/rtp_client.cpp
@@ -217,6 +217,7 @@ void RtpClient::rtpVideoRunner(RtpClient* self)
         auto rtp = reinterpret_cast<rtc::RtpHeader*>(buffer);
 
         {
+            std::lock_guard<std::mutex> lock(self->mutex_);
             for (const auto& [key, value] : self->mediaTracks_) {
                 rtp->setSsrc(value.ssrc);
                 rtp->setPayloadType(value.dstPayloadType);
