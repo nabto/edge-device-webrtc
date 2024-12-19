3fd96cd9d773f6cfe1c98d4917374a4ab4d42b19
Michael Madsen <mkm85@users.noreply.github.com>
2024-12-19 10:52:25 +0100

    update libdatacnannel and other libraries (#59)

--- build-scripts/vcpkg/ports/libdatachannel/portfile.cmake
+++ build-scripts/vcpkg/ports/libdatachannel/portfile.cmake
@@ -1,28 +1,21 @@
-set(PATCHES fix-for-vcpkg.patch)
-
-if(VCPKG_TARGET_IS_UWP)
-    list(APPEND PATCHES uwp-warnings.patch)
-endif()
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO paullouisageneau/libdatachannel
-    REF "1691c95a57de60b7c277e0cdf8d53351218c2c7c"
-    SHA512 2b57bf0160e87af2c98dd0c3aecfd38832ce2d1118711a03f1ae71a6ba9a451e7adb7e373d4b99534626627725d973e01d683778e8b3ed536d066fdbd9bdb9ca
+    REF "v${VERSION}"
+    SHA512 36804c7f50e71e93718a13d80315478aeef1bb05715ea157dedef2c9a40d7f391a5dcb4f4a1cd7482efd3c5dcd3940b0736b1de65ed3bda4e1a2471a4ec10bdb
     HEAD_REF master
     PATCHES
-        ${PATCHES}
-        fix_dependency.patch
+        dependencies.diff
+        #library-linkage.diff
+        uwp-warnings.patch
 )
-file(REMOVE "${SOURCE_PATH}/cmake/Modules/FindLibJuice.cmake")
-string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" DATACHANNEL_STATIC_LINKAGE)
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
         stdcall CAPI_STDCALL
     INVERTED_FEATURES
-        ws NO_WEBSOCKET
-        srtp NO_MEDIA
+        ws      NO_WEBSOCKET
+        srtp    NO_MEDIA
 )
 
 vcpkg_cmake_configure(
@@ -32,29 +25,17 @@ vcpkg_cmake_configure(
         -DPREFER_SYSTEM_LIB=ON
         -DNO_EXAMPLES=ON
         -DNO_TESTS=ON
-        -DDATACHANNEL_STATIC_LINKAGE=${DATACHANNEL_STATIC_LINKAGE}
 )
 
-vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(PACKAGE_NAME LibDataChannel CONFIG_PATH lib/cmake/LibDataChannel)
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/LibDataChannel)
 vcpkg_fixup_pkgconfig()
 
-if(srtp IN_LIST FEATURES)
-    set(FIND_DEP_SRTP "find_dependency(libSRTP CONFIG)")
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/rtc/common.hpp" "#ifdef RTC_STATIC" "#if 1")
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/rtc/rtc.h" "#ifdef RTC_STATIC" "#if 1")
 endif()
-
-file(READ "${CURRENT_PACKAGES_DIR}/share/LibDataChannel/LibDataChannelConfig.cmake" DATACHANNEL_CONFIG)
-file(WRITE "${CURRENT_PACKAGES_DIR}/share/LibDataChannel/LibDataChannelConfig.cmake" "
-include(CMakeFindDependencyMacro)
-find_dependency(Threads)
-find_dependency(OpenSSL)
-find_dependency(LibJuice)
-find_dependency(plog CONFIG)
-find_dependency(unofficial-usrsctp CONFIG)
-${FIND_DEP_SRTP}
-${DATACHANNEL_CONFIG}")
-
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
 
